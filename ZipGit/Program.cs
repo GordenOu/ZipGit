fbd362ba92f5e3a5b5220a2fcfe66ac41539b231
Gordon <qihao.ou@connect.ust.hk>
2020-07-18 17:01:14 +0800

    ZipGit.

---
+++ ZipGit/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Threading.Tasks;
+using Microsoft.DotNet.Cli.Utils;
+
+namespace ZipGit
+{
+    class Program
+    {
+        private static string[] Run(string commandName, params string[] args)
+        {
+            var lines = new List<string>();
+            var command = Command.Create(commandName, args);
+            Console.WriteLine($"{command.CommandName} {command.CommandArgs}");
+            var result = command.OnOutputLine(lines.Add).Execute();
+            if (result.ExitCode != 0)
+            {
+                Environment.Exit(result.ExitCode);
+            }
+            return lines.ToArray();
+        }
+
+        static async Task Main()
+        {
+            var lines = Run("git", "ls-files");
+            var directory = new DirectoryInfo(Directory.GetCurrentDirectory());
+            using var stream = new MemoryStream();
+            using (var archive = new ZipArchive(stream, ZipArchiveMode.Create, leaveOpen: true))
+            {
+                foreach (var line in lines)
+                {
+                    string filePath = Path.Combine(directory.FullName, line);
+                    Console.WriteLine(filePath);
+                    archive.CreateEntryFromFile(filePath, line);
+                }
+            }
+            string zipFilePath = Path.Combine(directory.FullName, $"{directory.Name}.zip");
+            using var zipFile = File.OpenWrite(zipFilePath);
+            stream.Seek(0, SeekOrigin.Begin);
+            await stream.CopyToAsync(zipFile);
+        }
+    }
+}
